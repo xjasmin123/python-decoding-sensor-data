efaddf6f9a2dfecd38d30bca10bec3b03daa7882
Jasmin-Dhaliwal <jasmin.dhaliwal.ext@bayer.com>
2023-03-16 16:23:18 +0100

    module 4

---
+++ sensor/temperature_info.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+from house_info import HouseInfo
+
+
+class TemperatureData(HouseInfo):
+    def _convert_data(self, data):
+        recs = []
+
+        for rec in data:
+            recs.append(int(rec, base=10))
+
+        return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("temperature", rec_area)
+        return self._convert_data(recs)
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("temperature, rec_date")
+
+        return self._convert_data(recs)
