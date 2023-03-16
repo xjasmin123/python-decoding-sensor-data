5b027ae0da821ce9e2c6c1e02715fff238ddb6c4
Jasmin-Dhaliwal <jasmin.dhaliwal.ext@bayer.com>
2023-03-16 09:28:25 +0100

    changed method call

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -18,13 +18,13 @@ print("Loaded records: {}".format(len(data)))
 house_info = HouseInfo(data)
 test_area = 1
 
-recs = house_info.get_data_by_area(field="id", rec_area=test_area)
+recs = house_info.get_data_by_area("id", rec_area=test_area)
 
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 
-recs = house_info.get_data_by_date(field="id", rec_date=test_date)
+recs = house_info.get_data_by_date("id", rec_date=test_date)
 
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
