901b6de328a3d1a8ccb6e37bd8ba4d8fb626e1f2
Jasmin-Dhaliwal <jasmin.dhaliwal.ext@bayer.com>
2023-03-17 10:41:02 +0100

    corrected get_data_by_date method

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -1,15 +1,12 @@
-from datetime import date
-
 from house_info import HouseInfo
-
+from datetime import date
 
 class TemperatureData(HouseInfo):
     def _convert_data(self, data):
         recs = []
-
         for rec in data:
+            # Convert string of integers into integers based 10
             recs.append(int(rec, base=10))
-
         return recs
 
     def get_data_by_area(self, rec_area=0):
@@ -18,5 +15,4 @@ class TemperatureData(HouseInfo):
 
     def get_data_by_date(self, rec_date=date.today()):
         recs = super().get_data_by_date("temperature", rec_date)
-
         return self._convert_data(recs)
